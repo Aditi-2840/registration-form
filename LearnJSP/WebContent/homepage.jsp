57656c4c41cf0b13f1de60cd252ff8c5f29bb27e
Aditi-2840 <aditikumari2840@gmail.com>
2022-01-18 18:55:55 +0530

    Added All Files

---
+++ LearnJSP/WebContent/homepage.jsp
@@ -0,0 +1,11 @@
+<html>
+
+<body>
+
+<jsp:include page="my-header.html"/>
+
+<jsp:include page="my-footer.jsp"/>
+
+</body>
+
+</html>
---
+++ registration-form/src/com/java/registration/model/Employee.java
@@ -0,0 +1,51 @@
+package com.java.registration.model;
+
+public class Employee {
+	
+	private String firstName;
+	private String lastName;
+	private String username;
+	private String password;
+	private String address;
+	private String contact;
+	public String getFirstname() {
+		return firstName;
+	}
+	public void setFirstname(String firstname) {
+		this.firstName = firstname;
+	}
+	public String getLastname() {
+		return lastName;
+	}
+	public void setLastname(String lastname) {
+		this.lastName = lastname;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getContact() {
+		return contact;
+	}
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+	
+	
+	
+
+}
